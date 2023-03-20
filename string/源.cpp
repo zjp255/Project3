2bd78661b0971461550b361eb31eda2e9e0c9e2d
zjp255 <95677971+zjp255@users.noreply.github.com>
2023-03-20 23:11:22 +0800

    add

---
+++ string/源.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+
+using namespace std;
+class MyString
+{
+public:
+	char* ch;
+	MyString(string str);
+	~MyString();
+	int getLength()
+	{
+		return length;
+	}
+
+	bool SubString(MyString& ms, int pos, int len)
+	{
+		if (pos > length || pos < 1 || pos + len - 1 > length)
+			return false;
+		for (size_t i = pos; i <= pos + len - 1; i++)
+		{
+			ms.ch[i - pos + 1] = ch[i];
+		}
+		ms.length = len;
+		return true;
+	}
+
+	int StrCompare(MyString &str1,MyString &str2)
+	{
+		for (size_t i = 1; i <= str1.length || i <= str2.length; i++)
+		{
+			if (str1.ch[i] != str2.ch[i])
+			{
+				char a1 = str1.ch[i];
+				char a2 = str2.ch[i];
+				return a1 - a2;
+			}
+		}
+		return str1.length - str2.length;
+	}
+
+	void printString()
+	{
+		for (size_t i = 1; i <= length; i++)
+		{
+			cout << ch[i];
+		}
+		cout << "\n";
+	}
+
+private:
+	
+	int length;
+	int MAXLEN = 255;
+};
+
+MyString::MyString(string str)
+{
+	length = 0;
+	ch = new char[MAXLEN]();
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		ch[i + 1] = str[i];
+		length++;
+	}
+}
+
+MyString::~MyString()
+{
+	delete ch;
+}
+
+int main()
+{
+	MyString str1 = MyString("my phone is redmik40");
+	MyString str2 = MyString("");
+
+	str1.SubString(str2, 5, 5);
+	str2.printString();
+
+	cout<<str1.StrCompare(str1, str2);
+	
+	return 0;
+}
