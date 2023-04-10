043deaa859a8863e805fbfb72c7b016a1e23a0e2
zjp255 <95677971+zjp255@users.noreply.github.com>
2023-04-10 16:44:28 +0800

    2023 4 10

--- BitTree/源.cpp
+++ BitTree/源.cpp
@@ -5,6 +5,7 @@ template<typename T>
 struct BitNode {
 	T elem;
 	bool isNull = true;
+	int ltag, rtag;
 	struct BitNode<T>* l_child;
 	struct BitNode<T>* r_child;
 };
@@ -15,7 +16,6 @@ class BitTree
 {
 public:
 	BitNode<T>* root;
-
 	BitTree<T>();
 	~BitTree<T>();
 
@@ -78,8 +78,15 @@ public:
 	}
 	void visit(BitNode<T>* n)
 	{
-		if (n->isNull == false)
+		if (n->elem == e  && isSelect == true)
+		{
+			selectNode = n;
+		}
+		else if (n->isNull == false)
+		{
+
 			cout << n->elem << endl;
+		}
 	}
 
 	void preOrder()
@@ -95,13 +102,72 @@ public:
 		postOrder(root);
 	}
 
+	void ThreadTree()
+	{
+		ThreadTree(root);
+		pre = NULL;
+
+	}
+
+	BitNode<T>* SelectNode(T e)
+	{
+		this->e = e;
+		isSelect = true;
+
+		inOrder();
+		isSelect = false;
+		return selectNode;
+	}
+
+	BitNode<T>* preNode(BitNode<T>* n)
+	{
+		if (n->ltag == 1)
+		{
+			return n->l_child;
+		}
+		else
+		{
+			BitNode<T>* temp;
+			temp = n->r_child;
+			while (temp->rtag == 0 && temp->r_child != NULL)
+			{
+				temp = temp->r_child;
+			}
+			return temp;
+		}
+	}
+
+	BitNode<T>* nextNode(BitNode<T>* n)
+	{
+		if (n->rtag == 1)
+		{
+			return n->r_child;
+		}
+		else
+		{
+			BitNode<T>* temp;
+			temp = n->l_child;
+			while (temp->ltag == 0 && temp->l_child != NULL)
+			{
+				temp = temp->l_child;
+			}
+			return temp;
+		}
+	}
 private:
+	BitNode<T>* pre;
+	BitNode<T>* selectNode;
+	T e;
+	bool isSelect = false;
+
 	void preOrder(BitNode<T>* n)
 	{
 		if (n != NULL)
 		{
 			visit(n);
+			if(n->ltag == 0 && n->isNull == false)
 			preOrder(n->l_child);
+			if (n->rtag == 0 && n->isNull == false)
 			preOrder(n->r_child);
 		}
 	}
@@ -109,22 +175,54 @@ private:
 	{
 		if (n != NULL)
 		{
-			
-			preOrder(n->l_child);
+			if (n->ltag == 0 && n->isNull == false)
+			inOrder(n->l_child);
 			visit(n);
-			preOrder(n->r_child);
+			if (n->rtag == 0 && n->isNull == false)
+			inOrder(n->r_child);
 		}
 	}
 	void postOrder(BitNode<T>* n)
 	{
 		if (n != NULL)
 		{
-			
-			preOrder(n->l_child);
-			preOrder(n->r_child);
+			if (n->ltag == 0 && n->isNull == false)
+			postOrder(n->l_child);
+			if (n->rtag == 0 && n->isNull == false)
+			postOrder(n->r_child);
 			visit(n);
 		}
 	}
+	void visitT(BitNode<T>* n)
+	{
+		if ( n->l_child == NULL ||n->l_child->isNull == true)
+		{
+			free(n->l_child);
+			n->l_child = pre;
+			n->ltag = 1;
+		}
+		
+		if (pre != NULL && (pre->r_child == NULL || pre->r_child->isNull == true))
+		{
+			free(pre->r_child);
+			pre->r_child = n;
+			pre->rtag = 1;
+		}
+		pre = n;
+	}
+	void ThreadTree(BitNode<T>* n)
+	{
+		if (n != NULL && n->isNull == false)
+		{
+			ThreadTree(n->l_child);
+			visitT(n);
+			ThreadTree(n->r_child);
+		}
+	}
+
+	
+
+	
 };
 template<typename T>
 BitTree<T>::BitTree()
@@ -149,5 +247,11 @@ int main()
 	bitTree->inOrder();
 	cout << "" << endl;
 	bitTree->postOrder();
+	bitTree->ThreadTree();
+	cout << "" << endl;
+	BitNode<int>* temp = bitTree->SelectNode(5);
+	cout << "" << endl;
+	cout << bitTree->preNode(temp)->elem << endl;
+	cout << bitTree->nextNode(temp)->elem << endl;
 	return 0;
 }
